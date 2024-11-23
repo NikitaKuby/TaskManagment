bd600bc6709fbdd78329a98fcd24594a4353048f
Nikita Kubyshkin <nikit666111@mail.ru>
2024-11-23 03:33:32 +0300

    v1.2.2 8~,9-

---
+++ src/main/java/com/example/taskmanagementsystem/domain/dto/Violation.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagementsystem.domain.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Violation {
+
+    private final String fieldName;
+    private final String message;
+
+}
---
+++ src/main/java/com/example/taskmanagementsystem/exceptions/UserWithoutAccessException.java
@@ -0,0 +1,4 @@
+package com.example.taskmanagementsystem.exceptions;
+
+public class UserWithoutAccessException extends RuntimeException{
+}
